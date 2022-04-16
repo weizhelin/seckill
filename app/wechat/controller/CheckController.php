295b021d75871243e81a49cc681a737d2c7c88e5
林盛志 <lsz@zbwlkj.net>
2022-04-16 23:40:52 +0800

    阶段提交1

--- app/wechat/controller/CheckController.php
+++ app/wechat/controller/CheckController.php
@@ -6,14 +6,15 @@ class CheckController
 
     public function do(){
         $config = [
-            'app_id' => 'wx0254e60d1dfbaf75',
-            'secret' => '429d18e21031140128649718870497b3',
-            'token' => 'TestToken',
+            'app_id' => '',
+            'secret' => '',
+            'token' => '',
             // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
             'response_type' => 'array',
 
             //...
         ];
+        $config = array_merge($config,config('wechat'));
 
         $app = Factory::officialAccount($config);
 
